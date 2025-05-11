f1a77570577be9a814e933fd517ef07e3d7f34af
Andrei <mogilnyi.a@gmail.com>
2025-05-11 13:50:52 +0000

    collect dom snapshot on click in EventCollector

--- js/web/src/EventCollector/index.js
+++ js/web/src/EventCollector/index.js
@@ -134,14 +134,80 @@ export default class EventCollector {
   // pushes selector to the array
   #handleClick(event) {
     const selector = DOMPath.xPath(event.target, true);
+    const snapshot = this.#collectDomSnapshot(event.target);
+    const current_url = window.location.href;
 
     const clickEvent = {
       event: selector,
       is_raw: true,
+      snapshot,
+      current_url,
     };
 
     this.events.push(clickEvent);
     window.localStorage.setItem("userlensEvents", JSON.stringify(this.events));
+  }
+
+  #collectDomSnapshot(el) {
+    const snapshot = [];
+    let current = el;
+
+    while (current && current !== document.documentElement) {
+      // get tagName and inner text if present
+      const tagName = current.tagName.toLowerCase();
+      // const text = current.innerText?.trim() || null;
+      // get element classList and element id
+      const classList = current.classList.length
+        ? [...current.classList]
+        : null;
+      const attr_id = current.id || null;
+
+      // get current element nth-child and ntf-of-type
+      const nth_child =
+        Array.from(current.parentNode?.children || []).indexOf(current) + 1;
+      const nth_of_type =
+        Array.from(current.parentNode?.children || [])
+          .filter((c) => c.tagName === current.tagName)
+          .indexOf(current) + 1;
+
+      // get current element element attributes
+      const attributes = {};
+      for (let attr of current.attributes) {
+        attributes[`attr__${attr.name}`] = attr.value;
+      }
+
+      // only allow text if this is one of the last 3 nodes (closest to the target)
+      const i = snapshot.length;
+      const includeText = i < 3;
+
+      const isClickable =
+        ["a", "button", "span", "label"].includes(tagName) ||
+        current.getAttribute("role") === "button" ||
+        current.getAttribute("tabindex") !== null ||
+        typeof current.onclick === "function" ||
+        (current.className &&
+          /btn|button|clickable|cta/.test(current.className));
+
+      const text =
+        includeText && isClickable ? current.innerText?.trim() || null : null;
+
+      // push to the beginning of the array
+      snapshot.unshift({
+        text,
+        tag_name: tagName,
+        attr_class: classList,
+        href: current.getAttribute("href"),
+        attr_id,
+        nth_child,
+        nth_of_type,
+        attributes,
+      });
+
+      // move on to the next element in dom tree
+      current = current.parentElement;
+    }
+
+    return snapshot;
   }
 
   pushEvent(event) {
