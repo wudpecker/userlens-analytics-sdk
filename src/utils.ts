b4646b05166138f5988d31d39756a694e2184978
Andrei <mogilnyi.a@gmail.com>
2025-09-08 14:49:46 +0000

    chore()

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ export function generateUuid(): string {
 }
 
 export function getUserlensVersion(): string {
-  return "0.1.64";
+  return "0.1.66";
 }
 
 export function saveWriteCode(writeCode: string) {
