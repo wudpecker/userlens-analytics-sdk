4020a60636686733c42f541c33c1967408d48ba8
Andrei <mogilnyi.a@gmail.com>
2025-09-21 15:13:03 +0000

    chore(repo): lower react version requirement

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ export function generateUuid(): string {
 }
 
 export function getUserlensVersion(): string {
-  return "0.1.69";
+  return "0.1.70";
 }
 
 export function saveWriteCode(writeCode: string) {
