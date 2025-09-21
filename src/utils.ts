6a037f9e3310994cf2b711e6793a529b55d3ccf1
Andrei <mogilnyi.a@gmail.com>
2025-09-21 15:03:39 +0000

    chore(repo):add npmignore file not to ship typescript source files

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ export function generateUuid(): string {
 }
 
 export function getUserlensVersion(): string {
-  return "0.1.68";
+  return "0.1.69";
 }
 
 export function saveWriteCode(writeCode: string) {
