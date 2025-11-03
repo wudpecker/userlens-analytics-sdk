32b7d1f4286f3e99545fc3104cb833370e9ed700
Andrei <mogilnyi.a@gmail.com>
2025-11-03 11:59:56 +0000

    chore(package): make react an optional peer dependency

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ export function generateUuid(): string {
 }
 
 export function getUserlensVersion(): string {
-  return "0.1.84";
+  return "0.1.85";
 }
 
 export function saveWriteCode(writeCode: string) {
